af967cf1929aee87278155566ad5912c74cb8741
Dehghan <dehghan9156@gmail.com>
2025-04-09 16:59:57 +0330

    add app exam

--- DjangoSmartExam/urls.py
+++ DjangoSmartExam/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
        path('admin/', admin.site.urls),
-       path("accounts/",include('accounts.urls')),
+       path("accounts/",include('accounts.urls',namespace='accounts')),
+       path("exam/",include('exam.urls',name='exam')),
 
 ]
