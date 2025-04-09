d2d150bf08b86fb356c449b988774223e88c7963
Dehghan <dehghan9156@gmail.com>
2025-04-09 11:18:49 +0330

    add model user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+type_person=(
+    (1,("Teacher")),
+    (2,("Student"))
+)
+
+class User(models.Model):
+    name = models.CharField(max_length=250)
+    type = models.CharField(choices=type_person)
